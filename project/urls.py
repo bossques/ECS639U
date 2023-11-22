100dc760e20d7bdf90a1f82278679cc5b92aa81d
TAJULAHMED <98593713+TAJULAHMED@users.noreply.github.com>
2023-11-22 14:08:25 +0000

    Removed the duplicate import

--- project/urls.py
+++ project/urls.py
@@ -17,8 +17,6 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.http import HttpResponse
-
-from django.conf import settings
 from django.conf.urls.static import static
 
 
